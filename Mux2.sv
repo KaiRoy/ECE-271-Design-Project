398388a5be11b6d52d1d4e09d171320a3d1797d8
Kai Roy <kai7lego+github@gmail.com>
2021-06-04 04:31:16 -0700

    Moved sv files
    Moved .sv files into a newly created SystemVerilog Folder

--- Mux2.sv
+++
@@ -1,8 +0,0 @@
-module mux2 (parameter N = 7, M = 7'b111_1111)
-    (input  logic [N-1:0]   d1,
-    input   logic           s,
-    output  logic [N-1:0]   y)
-    );
-
-    assign y = s ? d1 : M;
-endmodule
---
+++ SystemVerilog/PRNG.sv
@@ -0,0 +1,27 @@
+module prng#(parameter N = 24)
+				(input wire clk, reset, 
+				 output wire [N-1:0] q);
+
+	reg [N-1:0] random;
+	wire [N-1:0] next;
+	wire feedback;
+                        
+	always @(posedge clk, posedge reset)
+		begin 
+			if (reset)
+				begin
+					random <= 1;  
+        
+			end     
+		else if (clk == 1'b1)
+			random <= next;
+	end
+
+	
+	assign feedback = random[23] ^ random[22] ^ random[6] ^ random[2] ^ random[0];
+
+	assign next = {feedback, random[N-1:1]};
+
+	assign q = random;
+
+endmodule  
