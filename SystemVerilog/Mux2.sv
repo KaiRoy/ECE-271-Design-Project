32f71cb952366213811d0d7fa3ff5e450838bd74
Khanh Nguyen <71782997+Knguyen2107@users.noreply.github.com>
2021-06-04 10:23:31 -0700

    Update Mux2.sv

--- SystemVerilog/Mux2.sv
+++ SystemVerilog/Mux2.sv
@@ -1,7 +1,7 @@
-module mux2 (parameter N = 7, M = 7'b111_1111)
+module mux2 #(parameter N = 7, M = 7'b111_1111)
     (input  logic [N-1:0]   d1,
     input   logic           s,
-    output  logic [N-1:0]   y)
+    output  logic [N-1:0]   y
     );
 
     assign y = s ? d1 : M;
